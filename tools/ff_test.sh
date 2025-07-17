d15b2661a62f621353f8843eb6f7bc9bc48f2f48
Roman Serov <roman@serov.co>
2025-07-17 14:28:19 +0300

    Тестовый скрипт. Возможность прогонять тулзу на каждой платформе в отдельности

--- tools/ff_test.sh
+++ tools/ff_test.sh
@@ -6,9 +6,23 @@ then
     exit -1
 fi;
 
+if [ -z $2 ]
+then
+    echo "Specify platform (lowercase available):"
+    echo "[ EGOLD, SGOLD, SGOLD2, SGOLD2_ELKA, ALL ]"
+    exit -1
+fi;
+
 set -e
 
-FF_TEST_DIR_PATH=$1
+PLATFORM="${2,,}"
+
+FF_TEST_DIR_PATH=`realpath "${1}"`
+
+FF_TEST_DIR_EGOLD="$FF_TEST_DIR_PATH/EGOLD"
+FF_TEST_DIR_SGOLD="$FF_TEST_DIR_PATH/SGOLD"
+FF_TEST_DIR_SGOLD2="$FF_TEST_DIR_PATH/SGOLD2"
+FF_TEST_DIR_SGOLD2_ELKA="$FF_TEST_DIR_PATH/SGOLD2_ELKA"
 
 SCRIPT_PATH=${BASH_SOURCE[0]};
 SCRIPT_PATH_FULL=`realpath "${SCRIPT_PATH}"`
@@ -21,12 +35,68 @@ if [ ! -f ${NIGHTMAN_PATH} ]; then
     exit -1
 fi
 
-cd ${FF_TEST_DIR_PATH}
+check_dir() {
+    FILES=$(find . -maxdepth 1 -iname '*.bin')
 
-FILES=$(find . -maxdepth 1 -iname '*.bin')
+    for FILE in $FILES
+    do
+        echo Processing "$FILE"
+        $NIGHTMAN_PATH -l -o "$FILE"
+    done
+}
 
-for FILE in $FILES
-do
-	echo Processing "$FILE"
-	$NIGHTMAN_PATH -l -o "$FILE"
-done
+check_egold () {
+    cd ${FF_TEST_DIR_EGOLD}
+
+    check_dir
+}
+
+check_sgold () {
+    cd ${FF_TEST_DIR_SGOLD}
+
+    check_dir
+}
+
+check_sgold2 () {
+    cd ${FF_TEST_DIR_SGOLD2}
+
+    check_dir
+}
+
+check_sgold2_elka () {
+    cd ${FF_TEST_DIR_SGOLD2_ELKA}
+
+    check_dir
+}
+
+case $PLATFORM in
+    egold)
+        check_egold
+        ;;
+
+    sgold)
+        check_sgold
+        ;;
+
+    sgold2)
+        check_sgold2
+        ;;
+
+    sgold2_elka)
+        check_sgold2_elka
+        ;;
+
+    all)
+        check_egold
+        check_sgold
+        check_sgold2
+        check_sgold2_elka
+        ;;
+
+    *)
+        echo "Aavailable platforms (lowercase available):"
+        echo "[ EGOLD, SGOLD, SGOLD2, SGOLD2_ELKA, ALL ]"
+
+        exit -1
+        ;;
+esac
